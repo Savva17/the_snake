86e12f25fa4623f3fa40027ae8aae92372007bde
Савва Морозов <morsavvamorozov@yandex.ru>
2024-06-25 15:29:13 +0300

    add change

--- the_snake.py
+++ the_snake.py
@@ -87,7 +87,6 @@ class Apple(GameObject):
 
 class Snake(GameObject):
     """Настройка характеристик змейки."""
-    
     def __init__(self):
         """Атрибуты дочернего класса Snake."""
         super().__init__(body_color=SNAKE_COLOR)
